bfc99cdb26daf82a5ace0d01270830711d8d81c9
praneeth <gp.gunturu@gmail.com>
2025-09-05 12:57:08 -0500

    added navbar outline

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,25 @@
 import './Navbar.css'
 
+import logo from "../../Assets/Frontend_Assets/logo.png"
+import cart_icon from "../../Assets/Frontend_Assets/cart_icon.png"
+
 export default function Navbar() {
     return (
-        <div>
-            Navbar
+        <div className='navbar'>
+            <div className='nav-logo'>
+                <img src={logo} alt=''/>
+                <p>MERN-CART</p>
+            </div>
+            <ul className='nav-menu'>
+                <li>Shop</li>
+                <li>Men</li>
+                <li>Women</li>
+                <li>Kids</li>
+            </ul>
+            <div className='nav-login-cart'>
+                <button>Login</button>
+                <img src={cart_icon} alt=''/>
+            </div>
         </div>
     )
 }
