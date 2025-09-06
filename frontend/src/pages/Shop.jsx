3970806a31a55af66f38875250ec2a4f36898853
praneeth <gp.gunturu@gmail.com>
2025-09-06 14:16:24 -0500

    Added NewLetter component and its styles

--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -1,5 +1,6 @@
 import Hero from "../components/Hero/Hero";
 import NewCollections from "../components/NewCollections/NewCollections";
+import NewsLetter from "../components/NewsLetter.jsx/NewsLetter";
 import Offers from "../components/Offers/Offers";
 import Popular from "../components/Popular/Popular";
 
@@ -10,6 +11,7 @@ export default function Shop() {
             <Popular />
             <Offers />
             <NewCollections />
+            <NewsLetter />
         </div>
     )
 }
