6eaa784a32c8978fcbf78e8ec5ff15af5a9ba92d
praneeth <gp.gunturu@gmail.com>
2025-09-06 11:06:58 -0500

    Few mods

--- frontend/src/components/Offers/Offers.jsx
+++ frontend/src/components/Offers/Offers.jsx
@@ -5,7 +5,8 @@ export default function Offers() {
     return (
         <div className="offers">
             <div className="offers-left">
-                <h1>Exclusive <br />Offers For You</h1>
+                <h1>Exclusive</h1>
+                <h1>Offers For You</h1>
                 <p>ONLY ON BEST SELLER PRODUCTS</p>
                 <button>Check Now</button>
             </div>
