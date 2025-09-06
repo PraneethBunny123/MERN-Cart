d37ab8408d04f97e158c84335c9dedc9bb7109e6
praneeth <gp.gunturu@gmail.com>
2025-09-06 14:32:41 -0500

    Basic styles

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -10,28 +10,28 @@ export default function Footer() {
             <div className='footer-logo'>
                 <img src={footer_logo} alt=''/>
                 <p>MERN-CART</p>
-                <ul className='footer-links'>
-                    <li>Company</li>
-                    <li>Products</li>
-                    <li>Offices</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-                <div className='footer-social-icons'>
-                    <div className='footer-icons-container'>
-                        <img src={instagram_icon} alt=''/>
-                    </div>
-                    <div className='footer-icons-container'>
-                        <img src={pinterest_icon} alt=''/>
-                    </div>
-                    <div className='footer-icons-container'>
-                        <img src={whatsapp_icon} alt=''/>
-                    </div>
+            </div>
+            <ul className='footer-links'>
+                <li>Company</li>
+                <li>Products</li>
+                <li>Offices</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+            <div className='footer-social-icons'>
+                <div className='footer-icons-container'>
+                    <img src={instagram_icon} alt=''/>
                 </div>
-                <div className='footer-copyright'>
-                    <hr />
-                    <p>Copyright @2025 - Praneeth</p>
+                <div className='footer-icons-container'>
+                    <img src={pinterest_icon} alt=''/>
                 </div>
+                <div className='footer-icons-container'>
+                    <img src={whatsapp_icon} alt=''/>
+                </div>
+            </div>
+            <div className='footer-copyright'>
+                <hr />
+                <p>Copyright @2025 - Praneeth</p>
             </div>
         </div>
     )
