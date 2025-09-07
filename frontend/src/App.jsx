f0596e085a3cfcf895ab52b7fa03c55649840b0a
praneeth <gp.gunturu@gmail.com>
2025-09-06 22:25:53 -0500

    displaying products in each product category. outlined ShopCategory component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,11 @@ import Cart from "./pages/Cart"
 import LoginSignup from "./pages/LoginSignup"
 import Footer from "./components/Footer/Footer"
 
+import men_banner from "./Assets/Frontend_Assets/banner_mens.png"
+import women_banner from "./Assets/Frontend_Assets/banner_women.png"
+import kid_banner from "./Assets/Frontend_Assets/banner_kids.png"
+
+
 function App() {
     return (
         <div>
@@ -15,9 +20,9 @@ function App() {
                 <Navbar />
                 <Routes>
                     <Route path="/" element={<Shop />}/>
-                    <Route path="/mens" element={<ShopCategory category="men"/>}/>
-                    <Route path="/womens" element={<ShopCategory category="women"/>}/>
-                    <Route path="/kids" element={<ShopCategory category="kids"/>}/>
+                    <Route path="/mens" element={<ShopCategory banner={men_banner} category="men"/>}/>
+                    <Route path="/womens" element={<ShopCategory banner={women_banner} category="women"/>}/>
+                    <Route path="/kids" element={<ShopCategory banner={kid_banner} category="kid"/>}/>
                     <Route path="/product" element={<Product />}> 
                         <Route path=":productId" element={<Product />}/>
                     </Route>
--- frontend/src/pages/ShopCategory.jsx
+++ frontend/src/pages/ShopCategory.jsx
@@ -1,7 +1,31 @@
-export default function ShopCategory({category}) {
+import "./css/ShopCategory.css"
+import all_product from "../Assets/Frontend_Assets/all_product"
+import dropdown_icon from "../Assets/Frontend_Assets/dropdown_icon.png"
+import Item from "../components/Item/Item"
+
+export default function ShopCategory({category, banner}) {
+    const filteredProducts = all_product.filter(product => product.category === category)
+    
     return (
-        <div>
-            ShopCategory: {category}
+        <div className="shop-category">
+            <img src={banner} alt=""/>
+            <div className="shop-category-index-sort">
+                <p>
+                    <span>Showing 1-12</span>
+                     out of 36 products
+                </p>
+                <div className="shop-category-sort">
+                    Sort by <img src={dropdown_icon} alt=""/>
+                </div>
+            </div>
+            <div className="shop-category-products">
+                {filteredProducts.map(product => (
+                    <Item 
+                        key={product.id}
+                        {...product}
+                    />
+                ))}
+            </div>
         </div>
     )
 }
