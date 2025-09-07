57d447a458f7e23a9b78e613cf45be7989c39686
praneeth <gp.gunturu@gmail.com>
2025-09-06 22:38:49 -0500

    styled new adds

--- frontend/src/pages/ShopCategory.jsx
+++ frontend/src/pages/ShopCategory.jsx
@@ -26,6 +26,9 @@ export default function ShopCategory({category, banner}) {
                     />
                 ))}
             </div>
+            <div className="shop-category-loadmore">
+                Explore More
+            </div>
         </div>
     )
 }
