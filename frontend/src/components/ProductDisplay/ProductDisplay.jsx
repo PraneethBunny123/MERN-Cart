64b2f19e055a0fecfbe3ada18490dcb7cf60e321
praneeth <gp.gunturu@gmail.com>
2025-09-07 14:39:31 -0500

    Completed the outline of the individual product

--- frontend/src/components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/components/ProductDisplay/ProductDisplay.jsx
@@ -3,7 +3,7 @@ import star_icon from '../../Assets/Frontend_Assets/star_icon.png'
 import star_dull_icon from '../../Assets/Frontend_Assets/star_dull_icon.png'
 
 
-export default function ProductDisplay({image, name}) {
+export default function ProductDisplay({image, name, old_price, new_price}) {
     return (
         <div className='product-display'>
             <div className='product-display-left'>
@@ -28,6 +28,29 @@ export default function ProductDisplay({image, name}) {
                     <img src={star_dull_icon} alt='' />
                     <p>(39)</p>
                 </div>
+                <div className='product-display-right-prices'>
+                    <div className='product-display-right-price-old'>${old_price}</div>
+                    <div className='product-display-right-price-new'>${new_price}</div>
+                </div>
+                <div className='product-display-right-description'>
+                    Elegant and versatile women’s blouse crafted from soft, breathable fabric. Features a flattering fit, modern design, and lightweight comfort—perfect for both casual outings and office wear.
+                </div>
+                <div className='product-display-right-size'>
+                    <h1>Select Size</h1>
+                    <div className='product-display-right-size'>
+                        <div>S</div>
+                        <div>M</div>
+                        <div>L</div>
+                        <div>XL</div>
+                    </div>
+                </div>
+                <button>Add to Cart</button>
+                <p className='product-display-right-category'>
+                    <span>Category :</span>Women, T-Shirt, Crop Top
+                </p>
+                <p className='product-display-right-category'>
+                    <span>Tags :</span>Modren, Casual
+                </p>
             </div>
         </div>
     )
