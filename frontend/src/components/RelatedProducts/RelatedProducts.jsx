e1f7f1d087200bd469a42c8108e623e706b34d0b
praneeth <gp.gunturu@gmail.com>
2025-09-08 15:35:52 -0500

    created a related products component

---
+++ frontend/src/components/RelatedProducts/RelatedProducts.jsx
@@ -0,0 +1,20 @@
+import "./RelatedProducts.css"
+import data_product from "../../Assets/Frontend_Assets/data"
+import Item from "../Item/Item"
+
+export default function RelatedProducts() {
+    return (
+        <div className="related-products">
+            <h1>Related Products</h1>
+            <hr />
+            <div className="related-products-item">
+                {data_product.map(item => (
+                    <item 
+                        key={item.id}
+                        {...item}
+                    />
+                ))}
+            </div>
+        </div>
+    )
+}
