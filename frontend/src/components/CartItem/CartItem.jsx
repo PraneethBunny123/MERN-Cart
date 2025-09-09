e061feccb4a48653593b03b8cfacda53df550c19
praneeth <gp.gunturu@gmail.com>
2025-09-09 16:31:36 -0500

    few styles for cart page

--- frontend/src/components/CartItem/CartItem.jsx
+++ frontend/src/components/CartItem/CartItem.jsx
@@ -22,13 +22,18 @@ export default function CartItem() {
                 if(productQuantity > 0) {
                     return (
                         <div key={product.id}>
-                            <div className='cart-item-format'>
+                            <div className='cart-item-format cart-item-main'>
                                 <img src={product.image} alt='' className='cart-item-product-icon'/>
                                 <p>{product.name}</p>
                                 <p>{product.new_price}</p>
                                 <button className='cart-item-quantity'>{productQuantity}</button>
                                 <p>{product.new_price * productQuantity}</p>
-                                <img src={remove_icon} alt='' onClick={() => removeFromCart(product.id)}/>
+                                <img 
+                                    src={remove_icon} 
+                                    alt='' 
+                                    onClick={() => removeFromCart(product.id)}
+                                    className='cart-item-remove'    
+                                />
                             </div>
                             <hr />
                         </div>
