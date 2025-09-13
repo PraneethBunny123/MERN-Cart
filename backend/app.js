bdefc735087e71158f43073269d7f736c70d8861
praneeth <gp.gunturu@gmail.com>
2025-09-13 17:22:11 -0500

    setup backend: installed express, jwt, multer, cors, mongoose. Connected to mongodb atlas and express server in running!

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+const port = 4000
+
+const express = require("express")
+const app = express()
+require('dotenv').config(); // for env imports
+const mongoose = require("mongoose")
+const jwt = require("jsonwebtoken")
+const multer = require("multer")
+const path = require("path")
+const cors = require("cors")
+
+app.use(express.json())
+app.use(cors())
+
+// mongodb connection 
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("connected to database successfully"))
+.catch(err => console.log(err))
+
+// API creation
+
+app.get("/", (req, res) => {
+    res.send("express app is running")
+})
+
+app.listen(port, (err) => {
+    if(!err) {
+        console.log("server running on port: "+ port)
+    } else {
+        console.log("error"+ err)
+    }
+})
