78356408b2e32acf9f59a1799a252889aa97386d
praneeth <gp.gunturu@gmail.com>
2025-09-14 14:56:59 -0500

    created upload endpoints

--- backend/app.js
+++ backend/app.js
@@ -23,11 +23,24 @@ app.get("/", (req, res) => {
     res.send("express app is running")
 })
 
+// Image storage engine
 const storage = multer.diskStorage({
-    destination: './assets/images',
+    destination: './upload/images',
     filename: (req, file, cb) => {
         return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
     }
+})
+
+const upload = multer({storage: storage})
+
+// Creating upload endpoint for images
+app.use('/images', express.static('/upload/images'))
+
+app.post('/upload', upload.single('product'), (req, res) => {
+    res.json({
+        success: 1,
+        image_url: `http://localhost${port}/images/${req.file.filename}`
+    })
 })
 
 app.listen(port, (err) => {
