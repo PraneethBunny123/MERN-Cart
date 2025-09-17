7d2be4bcd7e6d327a9c088093fff03efbfc009ba
praneeth <gp.gunturu@gmail.com>
2025-09-17 12:56:55 -0500

    Created simple navbar with basic styles

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,13 @@
 import './Navbar.css'
+import nav_logo from "../../assets/Admin_Assets/nav-logo.svg"
+import nav_profile from "../../assets/Admin_Assets/nav-profile.svg"
+
 
 export default function Navbar() {
     return (
-        <div>
-            Navbar
+        <div className='navbar'>
+            <img src={nav_logo} alt='' className='nav-logo'/>
+            <img src={nav_profile} alt='' className='nav-profile'/>
         </div>
     )
 }
