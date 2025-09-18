e2fe1761027714800b8748754abe44198a6087ff
praneeth <gp.gunturu@gmail.com>
2025-09-17 21:05:24 -0500

    getting product details in the state

--- admin/src/components/AddProduct/AddProduct.jsx
+++ admin/src/components/AddProduct/AddProduct.jsx
@@ -4,30 +4,64 @@ import { useState } from 'react'
 
 export default function AddProduct() {
     const [image, setImage] = useState(null)
+    const [productDetails, setProductDetails] = useState({
+        name: "",
+        image: "",
+        category: "women",
+        old_price: "",
+        new_price: ""
+    })
 
     function handleImage(e) {
         setImage(e.target.files[0])
+    }
+
+    function handleProductDetails(e) {
+        setProductDetails(prevState => ({...prevState, [e.target.name]: e.target.value}))
     }
 
     return (
         <div className='add-product'>
             <div className='add-product-itemfield'>
                 <p>Product Title</p>
-                <input type='text' name='name' placeholder='Enter Product Name'/>
+                <input 
+                    type='text' 
+                    name='name' 
+                    placeholder='Enter Product Name'
+                    value={productDetails.name}
+                    onChange={handleProductDetails}    
+                />
             </div>
             <div className='add-product-price'>
                 <div className='add-product-itemfield'>
                     <p>Price</p>
-                    <input type='text' name='old_price' placeholder='Enter Old Price'/>
+                    <input 
+                        type='text' 
+                        name='old_price' 
+                        placeholder='Enter Old Price'
+                        value={productDetails.old_price}
+                        onChange={handleProductDetails}    
+                    />
                 </div>
                 <div className='add-product-itemfield'>
                     <p>Offer Price</p>
-                    <input type='text' name='new_price' placeholder='Enter New Price'/>
+                    <input 
+                        type='text' 
+                        name='new_price' 
+                        placeholder='Enter New Price'
+                        value={productDetails.new_price}
+                        onChange={handleProductDetails}    
+                    />
                 </div>
             </div>
             <div className='add-product-itemfield'>
                 <p>Product Category</p>
-                <select name='category' className='add-product-select'>
+                <select 
+                    name='category' 
+                    className='add-product-select'
+                    value={productDetails.category}
+                    onChange={handleProductDetails}
+                >
                     <option value="women">Women</option>
                     <option value="men">Men</option>
                     <option value="kid">Kid</option>
@@ -37,7 +71,13 @@ export default function AddProduct() {
                 <label htmlFor='file-input'>
                     <img src={image? URL.createObjectURL(image) : upload_area} alt='' className='add-product-thumbnail-image'/>
                 </label>
-                <input onChange={handleImage} type='file' name='image' id='file-input' hidden/>
+                <input 
+                    onChange={handleImage} 
+                    type='file' 
+                    name='image' 
+                    id='file-input' 
+                    hidden
+                />
             </div>
             <button className='add-product-button'>ADD</button>
         </div>
