635e44c355eac5084896e6e1ceb18cbaf5fc7ad3
praneeth <gp.gunturu@gmail.com>
2025-09-23 17:35:15 -0500

    api endpoint for login: creating jwt token if email, password matches

--- backend/app.js
+++ backend/app.js
@@ -134,6 +134,34 @@ app.post('/signup', async (req, res) => {
     })
 })
 
+// endpoint for user login
+app.post('/login', async (req, res) => {
+    let user = await User.findOne({email: req.body.email})
+    if(user) {
+        const passCompare = req.body.password === user.password
+        if(passCompare) {
+            const data = {
+                user: {id: user.id}
+            }
+            const token = jwt.sign(data, 'secret_ecom')
+            res.json({
+                success: true,
+                token
+            })
+        } else {
+            res.json({
+                success: false,
+                error: 'Wrong Password'
+            })
+        }
+    } else {
+        res.json({
+            success: false,
+            error: 'Wrong email id'
+        })
+    }
+})
+
 app.listen(port, (err) => {
     if(!err) {
         console.log("server running on port: "+ port)
