8459cc1fe7b547166b680622f77f05e9505864fc
praneeth <gp.gunturu@gmail.com>
2025-09-23 18:09:58 -0500

    Making the loginSignup page interactive, making usre it will handle both login and signup

--- frontend/src/pages/LoginSignup.jsx
+++ frontend/src/pages/LoginSignup.jsx
@@ -1,19 +1,24 @@
+import { useState } from "react"
 import "./css/LoginSignup.css"
 
 export default function LoginSignup() {
+    const [auth, setAuth] = useState('Sign Up')
+
     return (
         <div className="loginsignup">
             <div className="loginsignup-container">
-                <h1>Sign Up</h1>
+                <h1>{auth}</h1>
                 <div className="loginsignup-fields">
-                    <input type="text" placeholder="Your Name"/>
+                    {auth === 'Sign Up' && <input type="text" placeholder="Your Name"/>}
                     <input type="email" placeholder="Email Address"/>
                     <input type="password" placeholder="Password"/>
                 </div>
                 <button>Continue</button>
-                <p className="loginsignup-login">
-                    Already have an account? <span>Login here</span>
-                </p>
+                {auth === 'Sign Up' ? (
+                    <p className="loginsignup-login">Already have an account? <span onClick={() => setAuth('Login')} style={{cursor: "pointer"}}>Login here</span></p>
+                ) : (
+                    <p className="loginsignup-login">New User? <span onClick={() => setAuth('Sign Up')} style={{cursor: "pointer"}}>Sign Up</span></p>
+                )}
                 <div className="loginsignup-agree">
                     <input type="checkbox" name="" id="" />
                     <p>By continuing, I agree to the terms of use & privacy policy</p>
