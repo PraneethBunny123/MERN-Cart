60051633f514a443c31558865c8ec3d7e8c2d408
praneeth <gp.gunturu@gmail.com>
2025-09-23 16:23:51 -0500

    created userSchema to store the user details in mongodb

---
+++ backend/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    name: {
+        type: String
+    },
+    email: {
+        type: String,
+        unique: true
+    },
+    password: {
+        type: String
+    },
+    cartData: {
+        type: Object
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+
+module.exports = mongoose.model('User', userSchema)
